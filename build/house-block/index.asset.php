4a72a45f625951dcc0cec32a5b23fc68c3c5dd89
Kailey Lampert <trepmal@gmail.com>
2025-08-10 18:54:01 -0600

    ignore built files

--- build/house-block/index.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('react', 'react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '9064c3e3c851f7d3d519');
--- build/house-block/view.asset.php
+++
@@ -1,1 +0,0 @@
-<?php return array('dependencies' => array('@wordpress/interactivity'), 'version' => 'e5751a14653af528a2d9', 'type' => 'module');
